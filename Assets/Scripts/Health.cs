27c62533e2ca6a9c7dadd530d0bae04ece5bc3f6
Ortreke <vczirkunov2001@mail.ru>
2022-05-20 01:14:52 +0300

    Release gameplay
    Without sounds

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,7 +5,19 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [SerializeField] bool isPlayer;
     [SerializeField] int health = 50;
+    [SerializeField] int score = 50;
+
+    ScoreKeeper scoreKeeper;
+    LevelManager levelManager;
+
+    void Awake()
+    {
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+        levelManager = FindObjectOfType<LevelManager>();
+    }
+
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -19,13 +31,32 @@ public class Health : MonoBehaviour
         }
     }
 
+    public int GetHealth()
+    {
+        return health;
+    }
+
     void TakeDamage(int damage)
     {
         health -= damage;
 
         if (health <= 0)
         {
-            Destroy(gameObject);
+            Die();
         }
     }
+
+    void Die()
+    {
+        if (!isPlayer)
+        {
+            scoreKeeper.ModifyScore(score);
+        }
+        else
+        {
+            levelManager.LoadGameOver();
+        }
+        Destroy(gameObject);
+    }
+
 }
---
+++ Assets/Scripts/UIDisplay.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIDisplay : MonoBehaviour
+{
+    [SerializeField] Health playerHealth;
+
+    [Header("Score")]
+    [SerializeField] TextMeshProUGUI scoreText;
+    ScoreKeeper scoreKeeper;
+
+    void Awake()
+    {
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        scoreText.text = scoreKeeper.GetScore().ToString("000000000");
+    }
+}
